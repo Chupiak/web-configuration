c62ee50b6ddaf849707410de75a764c3fb5678d5
mykolachupyak <chupyak@gmail.com>
2023-04-26 11:15:29 +0300

    Solution with @WebServlet

--- src/main/java/org/example/controller/IndexController.java
+++ src/main/java/org/example/controller/IndexController.java
@@ -3,11 +3,13 @@ package org.example.controller;
 
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
